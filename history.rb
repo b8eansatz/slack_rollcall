9981c934eef4b6cdef436846e591bcc93244da02
Tatsuya Toyoda <b8eansatz@gmail.com>
2015-02-17 16:30:16 +0900

    Add comments

--- history.rb
+++ history.rb
@@ -23,7 +23,6 @@ def channels_info(params)
 end
 
 def chat_postMessage(params)
-  #params[:channel] = ENV["CHANNEL"]
   params[:username] = ENV['USERNAME']
   path = "/api/chat.postMessage"
   geturi(path, params)
@@ -41,47 +40,37 @@ end
 
 Dotenv.load
 token = ENV['TOKEN']
-#oldest = Time.local(Time.now.year, Time.now.month, Time.now.day, 0, 0, 0).to_i
 params = {
   :token => token,
 }
 
+#GETS INFORMATION ABOUT A CHANNEL
 result_channel = JSON.parse(channels_info(params))
-result_history = JSON.parse(channels_history(params))
-messages = result_history["messages"]
-puts "#{result_channel}"
-#puts messages.length
-#puts "#{messages[0]}"
-#puts messages[0]["user"]
 
+#FETCHES HISTORY OF MESSAGES & EVENTS FROM A CHARACTER
+result_history = JSON.parse(channels_history(params))
+
+#GETS USERS EVER POSTED ON THE CHANNEL
+messages = result_history["messages"]
 attend = Array.new
-#puts "#{attend}"
 messages.each_with_index do |message, i|
   puts message["user"]
   attend << "#{message["user"]}"
 end
-
 attend = attend.uniq
-#attend.delete("")
-#puts "ATTENDS = #{attend}"
-members = result_channel["channel"]["members"]
-#puts "MEMBERS = #{members}"
-absent = members - attend
-#puts "ABSENTS = #{absent}"
 
+#GETS USERS NEVER POSTED ON THE CHANNEL
+members = result_channel["channel"]["members"]
+absent = members - attend
+
+#SENDS MESSAGES TO EACH USERS
 attend.each_with_index do |user|
   unless user.empty? == true then
     params[:text] = "<@#{user}>: Good morning!"
     chat_postMessage(params)
   end
 end
-
-#absent.each_with_index do |user|
-#  params[:text] = "<@#{user}>: How's it going?"
-#  chat_postMessage(params)
-#end
-
-#puts "MEMBERS = #{result_channel["channel"]["members"]}"
-#puts "#{result_history}"
-#puts "#{result_history.assoc("messages")}"
-#puts "MESSAGES = #{result_history["messages"]}"
+absent.each_with_index do |user|
+  params[:text] = "<@#{user}>: How's it going?"
+  chat_postMessage(params)
+end
